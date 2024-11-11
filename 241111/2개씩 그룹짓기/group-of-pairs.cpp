56e2cf90797eee07bc505fcd97e75f1686674341
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-11-11 19:45:23 +0900

    Title: 2개씩 그룹짓기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241111/2개씩 그룹짓기/group-of-pairs.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+int n;
+int arr[1004];
+int main() {
+    // 여기에 코드를 작성해주세요.
+    
+    cin >> n ; 
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr,arr+n);
+
+    int l = 0 ;
+    int r = n-1;
+
+    int mx = 0 ;
+    while(l<=r)
+    {
+        mx = max(mx,arr[l]+ arr[r]);
+        l++;
+        r--;
+    }
+    cout << mx;
+    return 0;
+}
+
+//1 2 3 5 7 8
