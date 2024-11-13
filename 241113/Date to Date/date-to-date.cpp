13dd07d75a943e799395aa610f486873bc118387
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-11-13 17:21:28 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241113/Date to Date/date-to-date.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+int numofmonth[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+int main() {
+
+
+    int m1,m2,d1,d2;
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    int day= d2;
+    int totalday = 0 ;
+    while(1)
+    {
+        if(m1 == m2 && day == d2) break;
+        day++;
+        totalday++;
+
+        if(numofmonth[m1]< day)
+        {
+            m1++;
+            day = 1;
+        }
+    }
+    cout <<totalday ; 
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
