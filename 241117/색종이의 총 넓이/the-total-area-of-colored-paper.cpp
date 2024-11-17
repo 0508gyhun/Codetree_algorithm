176237a6945033c762c579328420fb61082a2d1e
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-11-17 20:06:49 +0900

    Title: 색종이의 총 넓이, Time: 16ms, Memory: 0MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241117/색종이의 총 넓이/the-total-area-of-colored-paper.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int a[204][204];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    cin >> n ; 
+    for(int i = 0 ; i < n; i ++)
+    {
+        int x1,y1,x2,y2;
+
+        cin >> x1>>y1;
+        
+        x2 = x1+8;
+        y2 = y1+8;   
+        
+
+        for(int j = y1 ; j <y2 ; j++)
+        {
+            for(int p = x1 ; p< x2 ; p++)
+            {
+                a[j][p] = 1;
+            }
+        }
+        
+    }
+    int cnt = 0 ;
+    for(int i = 0 ; i < 204 ; i++)
+    {
+        for(int j = 0 ; j < 204 ; j++)
+        {
+            if(a[i][j] ==1) cnt++;
+        }
+    }
+
+    cout << cnt;
+    
+    return 0;
+}
