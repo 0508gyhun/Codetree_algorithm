fdd6b0679894a596f8cfa7179e3b8bddf7d69790
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-11-19 14:02:32 +0900

    Title: 연속되는 수 2, Time: 16ms, Memory: 0MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241119/연속되는 수 2/continuous-number2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int n; 
+int a[1004];
+vector<int> v;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n ;
+    int cnt = 0 ; 
+    int tmp;
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin >> tmp;
+        a[i] = tmp;
+    }
+    for(int i = 0 ; i < n ; i++)
+    {
+        if(i == 0 || a[i-1] == a[i]){
+            cnt++;
+        }
+        else{
+            v.push_back(cnt);
+            cnt = 1;
+        }
+    }
+
+    int ans  = *max_element(v.begin(), v.end());
+
+    cout << ans;
+    // for(int i : v){
+    //     cout << i <<" ";
+    // }
+    return 0;
+}
