6006ffc1bd748c10f03ff61757e8985743dac59a
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-11-19 15:40:51 +0900

    Title: 만나는 그 순간, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241119/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int n,m;
+int a[1000004];
+int b[1000004];
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+
+    cin >> n >>m;
+    int cur = 0;
+    int time = 0;
+    for(int i = 0 ; i < n ; i++)
+    {   
+        char d;
+        int t;
+
+        cin >> d>>t;
+        
+        if(d == 'R')
+        {
+            for(int j = 0 ; j < t ; j++)
+            {
+               
+                a[time] = cur;
+                time++;
+                cur++;
+            }
+        }
+        else if( d == 'L')
+        {
+             for(int j = 0 ; j < t ; j++)
+            {
+                
+                a[time] = cur;
+                time++;
+                cur--;
+            }
+
+        }
+    }
+     a[time]=cur;
+
+    
+    //for(int i = 0 ; i <= 17 ; i++) cout << a[i]<<" ";    
+
+
+    cur = 0;
+    time = 0;
+    for(int i = 0 ; i < m ; i++)
+    {   
+        char d;
+        int t;
+
+        cin >> d>>t;
+        
+        if(d == 'R')
+        {
+            for(int j = 0 ; j < t ; j++)
+            {
+               
+                b[time] = cur;
+                time++;
+                cur++;
+            }
+        }
+        else if( d == 'L')
+        {
+             for(int j = 0 ; j < t ; j++)
+            {
+                
+                b[time] = cur;
+                time++;
+                cur--;
+            }
+
+        }
+    }
+     a[time]=cur;
+
+   
+    for(int i = 1 ; i <= 1000004 ; i++)
+    {
+        if(a[i] == b[i]){
+            cout << i;
+            break;
+        }
+    }    
+
+
+
+
+
+    return 0;
+}
