eb26c027f1fb0f341cd616192c5fc8a741e80ac3
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-11-19 14:18:09 +0900

    Title: 연속되는 수 3, Time: 15ms, Memory: 0MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241119/연속되는 수 3/continuous-number3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+
+bool isplus(int n)
+{
+    if(n>=0) return true;
+    else return false;
+}
+int a[2004];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n ; 
+    cin >> n ; 
+
+    for(int i = 0 ; i < n ; i++)
+    {   
+        int tmp ;
+        cin >> tmp;
+        a[i] = tmp ; 
+    }
+
+    int cnt = 0 ;
+    vector <int> v;
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        if(i == 0 || isplus(a[i-1]) == isplus(a[i]))
+        {
+            cnt++;
+        }
+        else
+        {
+            v.push_back(cnt);
+            cnt = 1;
+        }
+    }
+
+    int ans = *max_element(v.begin(), v.end());
+    cout << ans;
+    return 0;
+}
