b57fce4f1ae6823b3b3c5a3813ccbbca34c9760a
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-11-22 12:41:58 +0900

    Title: 선두를 지켜라 3, Time: 8ms, Memory: 2MB, Status: Passed - CodeTree

--- 241122/선두를 지켜라 3/keep-the-lead-3.cpp
+++ 241122/선두를 지켜라 3/keep-the-lead-3.cpp
@@ -38,36 +38,12 @@ int main() {
             bcur++;
         }
     }
-    // cout << endl;
-    // for(int i = 0 ; i < bcur ; i++)
-    // {
-    //     cout << b[i] << " ";
-    // }
+ 
 
-    // cout << "bcur"<<bcur << endl;
-    int mmx = 0;
-    if(acur>bcur) mmx = acur;
-    else mmx = bcur;
-
-    // if(acur>bcur)
-    // {
-    //     for(int i = bcur ; i < acur ; i++)
-    //     {
-    //         a[i] = a[i-1];
-    //     }
-    // }
-    // else{
-    //     for(int i = acur ; i < bcur ; i++)
-    //     {
-    //         b[i] = b[i-1];
-    //     }
-    // }
-
-    // a=1,b=2,ab=3;
-   int changes = 0;
+  int changes = 0;
     int currentLeader = 0;  // 0: 초기상태, 1: A리더, 2: B리더, 3: 동률
     
-    for(int i = 1; i <= acur; i++) {
+    for(int i = 1; i < acur; i++) {
         int newLeader;
         if(a[i] > b[i]) {
             newLeader = 1;
