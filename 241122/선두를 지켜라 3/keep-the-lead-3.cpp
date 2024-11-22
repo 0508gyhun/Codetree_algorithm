cf4654d0e1eb6e928dfb2377ba16e11419c19de3
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-11-22 12:21:31 +0900

    Title: 선두를 지켜라 3, Time: 13ms, Memory: 4MB, Status: Wrong Answer - CodeTree

--- 241122/선두를 지켜라 3/keep-the-lead-3.cpp
+++ 241122/선두를 지켜라 3/keep-the-lead-3.cpp
@@ -8,38 +8,76 @@ int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n>> m;
 
-    int cur = 1;
+    int acur = 1;
     for(int i = 0 ; i < n ; i++)
     {
         int v,t;
         cin >> v>>t;
         while(t--)
         {
-            a[cur] = a[cur-1]+v;
-            cur++;
+            a[acur] = a[acur-1]+v;
+            acur++;
         }
     }
 
-    for(int i = 0 ; i < cur ; i++)
-    {
-        cout << a[i] << " ";
-    }
+    // for(int i = 0 ; i < acur ; i++)
+    // {
+    //     cout << a[i] << " ";
+    // }
+    // cout << "acur"<<acur << endl;
 
-    cur = 1;
+
+    int bcur = 1;
     for(int i = 0 ; i < n ; i++)
     {
         int v,t;
         cin >> v>>t;
         while(t--)
         {
-            b[cur] = b[cur-1]+v;
-            cur++;
+            b[bcur] = b[bcur-1]+v;
+            bcur++;
         }
     }
-    cout << endl;
-    for(int i = 0 ; i < cur ; i++)
+    // cout << endl;
+    // for(int i = 0 ; i < bcur ; i++)
+    // {
+    //     cout << b[i] << " ";
+    // }
+
+    // cout << "bcur"<<bcur << endl;
+    int mmx = 0;
+    if(acur>bcur) mmx = acur;
+    else mmx = bcur;
+
+    // a=1,b=2,ab=3;
+    int cnt = 0 ;
+    int leader = 0 ;
+    for(int i = 1 ; i < bcur ; i++)
     {
-        cout << b[i] << " ";
+        if(a[i] > b[i]){
+            if(leader != 1) {
+                cnt++;
+              //  cout << i << endl;
+            }
+            leader = 1;
+        }
+        else if( a[i] < b[i])
+        {
+            if(leader != 2) {
+                cnt++;
+              //  cout << i << endl;
+            }
+            leader = 2;
+        }
+        else if ( a[i] == b[i])
+        {
+            if(leader != 3) {
+                cnt++;
+              //  cout << i << endl;
+            }
+            leader =3;
+        }
     }
+    cout << cnt;
     return 0;
 }
