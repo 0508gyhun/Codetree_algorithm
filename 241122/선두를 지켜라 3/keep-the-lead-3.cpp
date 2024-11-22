bb1a7b3234a51ae3ad2f3cab87f1b81169284db2
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-11-22 12:27:31 +0900

    Title: 선두를 지켜라 3, Time: 13ms, Memory: 4MB, Status: Wrong Answer - CodeTree

--- 241122/선두를 지켜라 3/keep-the-lead-3.cpp
+++ 241122/선두를 지켜라 3/keep-the-lead-3.cpp
@@ -49,10 +49,24 @@ int main() {
     if(acur>bcur) mmx = acur;
     else mmx = bcur;
 
+    if(acur>bcur)
+    {
+        for(int i = bcur ; i < acur ; i++)
+        {
+            a[i] = a[i-1];
+        }
+    }
+    else{
+        for(int i = acur ; i < bcur ; i++)
+        {
+            b[i] = b[i-1];
+        }
+    }
+
     // a=1,b=2,ab=3;
     int cnt = 0 ;
     int leader = 0 ;
-    for(int i = 1 ; i < bcur ; i++)
+    for(int i = 1 ; i < mmx ; i++)
     {
         if(a[i] > b[i]){
             if(leader != 1) {
