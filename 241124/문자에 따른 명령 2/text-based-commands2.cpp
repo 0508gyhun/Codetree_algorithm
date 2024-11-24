5592ba8dc656cd3f248d78af4258da9071a97245
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-11-24 12:19:31 +0900

    Title: 문자에 따른 명령 2, Time: 17ms, Memory: 0MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241124/문자에 따른 명령 2/text-based-commands2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+
+int dy[4] = {0,-1,0,1};
+int dx[4] = {1,0,-1,0};
+string s;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> s;
+    int dir = 3;
+    int x = 0;
+    int y = 0;
+    int ny=0;
+    int nx = 0 ;
+
+    for(int i = 0 ;i< s.size() ; i++)
+    {
+        if(s[i] == 'L')
+        {
+            dir = (dir-1)%4;
+        }
+        else if( s[i] == 'R')
+        {
+            dir = (dir+1)%4;
+        }
+        else if( s[i] == 'F')
+        {
+            ny = y+ dy[i];
+            nx = x+ dx[i];
+        }
+    }
+    cout << ny<< " "<< nx;
+    
+    
+    return 0;
+
+}
