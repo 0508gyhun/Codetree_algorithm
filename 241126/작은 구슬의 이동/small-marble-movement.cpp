63765636c8ca6cff2e564d42b4c5cfd0e9e07a95
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-11-26 13:17:18 +0900

    Title: 작은 구슬의 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241126/작은 구슬의 이동/small-marble-movement.cpp
+++ 241126/작은 구슬의 이동/small-marble-movement.cpp
@@ -1,39 +1,71 @@
 #include <iostream>
 using namespace std;
 
-int r,c; 
-char d; 
-int n, t; 
-int dy[4] = {0,-1,1,0}; 
-int dx[4] = {1,0,0,-1};  
+int r,c;
+char d;
+int n, t; // r d u l
+int dy[4] = {0,-1,1,0};
+int dx[4] = {1,0,0,-1};
 
-int getNum(char d) {     
-    if(d == 'L') return 3;     
-    else if(d == 'R') return 0;     
-    else if(d == 'U') return 1;     
-    else if(d == 'D') return 2; 
-}   
+int getNum(char d)
+{
+    if(d == 'L') return 3;
+    else if( d == 'R') return 0;
+    else if (d == 'D') return 2;
+    else if (d == 'U') return 1;
+}
 
-int main() {     
-    cin >> n >> t;      
-    cin >> r >> c >> d;     
 
-    int dir = getNum(d);     
-    int y = r-1;     
-    int x = c-1;       
+int main() {
+    // 여기에 코드를 작성해주세요.
+    
+    cin >> n >>t ; 
+    cin >> r >> c >>d;
+    // cout << "d "<<d<<endl;
 
-    for(int i = 0 ; i < t ; i++) {                      
-        int ny = y + dy[dir];         
-        int nx = x + dx[dir];         
+    int dir = getNum(d);
+    // cout << "dir " << dir<<endl;
 
-        if(ny < 0 || ny >= n || nx < 0 || nx >= n) {
-            dir = (dir + 2) % 4;  // 180도 방향 전환
+    int y = r-1;
+    int x = c-1;
+
+     cout << y << " "<< x<<endl;
+   
+    
+    for(int i = 0 ; i < t ; i++)
+    {   
+        
+
+        int ny = y + dy[dir];
+        int nx = x + dx[dir];
+        if(ny<0 || ny >= n || nx <0 || nx>= n){
+            dir = 3-dir;
             continue;
-        }         
-        y = ny;         
-        x = nx;     
-    }      
+        }
+        y = ny;
+        x = nx;
+        cout <<i<<" "<< y << " "<< x<<endl;
+    }
+     cout << y+1 <<" "<< x+1 << endl;
+        
+    // }
+    //     else if(d == 'U')
+    //     {
 
-    cout << y+1 << " " << x+1 << endl;     
-    return 0; 
+    //     }
+    //     else if(d == 'R')
+    //     {
+
+    //     }
+    //     elseif(d == 'D')
+   
+    
+    // for(int i = 0 ; i < n ; i ++)
+    // {
+    //     for(int j = 0 ; j < n ; j ++)
+    //     {
+    //         if(i==r)
+    //     }
+    // }
+    return 0;
 }
