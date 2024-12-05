082da892f58bcf0bbc0da0179d0bfea8c8edb3a2
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-12-05 14:13:59 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241205/괄호 쌍 만들어주기 3/pair-parentheses-3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+string s;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> s;
+
+    int cnt = 0 ;
+    for(int i = 0 ; i < s.size() ; i++)
+    {
+        if(s[i] == '(')
+        {
+            for(int j = i+1 ; j < s.size() ; j++)
+            {
+                if(s[j] == ')') cnt++;
+            }
+        }   
+    }
+    cout << cnt;
+        return 0;
+}
