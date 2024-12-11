f601ce07c684133398d8bc883b000a082fdb3c96
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-12-11 10:57:59 +0900

    Title: 오목, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241211/오목/O-mok.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+int n=19;
+int a[23][23];
+
+//동 동북 북 북서 서 남서 남 동남
+int dy[8] = {0,-1,-1,-1,0,1,1,1};
+int dx[8] = {1,1,0,-1,-1,-1,0,1};
+
+bool isRange(int y, int x){
+    return y>=0 && y < n && x>= 0 && x<n;
+}
+
+int main() {
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        for(int j = 0 ; j < n ; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        for(int j = 0 ; j < n ; j++)
+        {
+            if(a[i][j] == 0) continue;
+
+            for(int k = 0 ; k < 8 ; k++) // dir
+            {
+                int cury = i;
+                int curt=1;
+                int curx = j;
+                while(1)
+                {
+                    int ny = cury + dy[k];
+                    int nx = curx + dx[k];
+                    if(!isRange(ny,nx))break;
+                    if(a[cury][curx] != a[ny][nx]) break;   
+                    curt++;
+                    curx = nx;
+                    cury = ny;
+                }
+                if(curt == 5)
+                {
+                    cout << a[i][j] << endl;
+					cout << i + 2 * dx[k] + 1 << " " << j + 2 * dy[k] + 1;
+					return 0;
+                }
+             
+
+            }
+        }
+    }
+
+    cout <<0;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
