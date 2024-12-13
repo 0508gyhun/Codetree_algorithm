74b4e781ffe0710681c793032ac0ed9aba332c93
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-12-13 13:41:18 +0900

    Title: G or H 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241213/G or H 3/G-or-H-3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int n,k;
+int num ;
+char pos;
+
+int arr[104];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >>k;
+
+    int mx = INT_MIN;
+    for(int i = 0 ; i <  n ; i++)
+    {
+        cin >> num >> pos;
+        if(pos == 'G')arr[num] = 1;
+        else arr[num] = 2;
+
+        mx = max(mx,num);
+    }
+
+    int mxx = INT_MIN;
+    for(int i= 1 ;i <= mx-k+1 ; i++)
+    {
+        int sum = 0 ;
+        for(int j = i ; j < i+k ; j++)
+        {
+            sum+=arr[j];            
+        }
+        mxx = max(mxx,sum);
+    }
+    cout << mxx;
+    return 0;
+}
