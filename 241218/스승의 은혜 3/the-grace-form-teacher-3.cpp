b8b4d6b0a547734e584268338a4a934c764bb24b
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-12-18 11:39:51 +0900

    Title: 스승의 은혜 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241218/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int a[1004][2];
+int tmp[1004];
+int n,b;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >>n >>b;
+
+    int sum = 0;
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin >> a[i][0] >> a[i][1];
+        tmp[i] = a[i][0] +a[i][1];
+        sum+=tmp[i];
+    }
+
+    sort(tmp,tmp+n);
+    int cnt = 0 ;
+    int mx = INT_MIN;
+    for(int i = 0 ; i < n ; i ++)
+    {
+        int tmpsum = 0;
+        int cnt = 0;
+        for(int j = 0 ; j < n ; j++)
+        {
+            if(j == i){
+                tmp[i] = tmp[i] - a[i][0]/2;
+            }
+
+            if(tmpsum + tmp[j]<b)
+            {
+                tmpsum += tmp[j];
+                cnt++;
+            }
+
+        }
+        tmp[i] = tmp[i]+ a[i][0]/2;
+        mx = max(mx,cnt);
+    }
+    cout << mx;
+    return 0;
+}
