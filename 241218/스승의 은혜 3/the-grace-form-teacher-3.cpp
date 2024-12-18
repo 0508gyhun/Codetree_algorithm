294e0b555c1dcdd1500a3cd08c3863ae8d86c9cc
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-12-18 11:57:13 +0900

    Title: 스승의 은혜 3, Time: 13ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241218/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 241218/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -3,45 +3,42 @@
 using namespace std;
 
 int a[1004][2];
+int tmp[1004];
 int n,b;
 
 int main() {
-    cin >> n >> b;
+    // 여기에 코드를 작성해주세요.
+    cin >>n >>b;
 
-    for(int i = 0; i < n; i++) {
+    int sum = 0;
+    for(int i = 0 ; i < n ; i++)
+    {
         cin >> a[i][0] >> a[i][1];
+        tmp[i] = a[i][0] +a[i][1];
+        sum+=tmp[i];
     }
 
-    int mx = 0;
-    for(int i = 0; i < n; i++) {
+    
+    int mx = INT_MIN;
+    for(int i = 0 ; i < n ; i ++)
+    {
         int tmpsum = 0;
         int cnt = 0;
-        
-        // 선물 가격+배송비 배열 생성
-        int tmp[1004];
-        for(int j = 0; j < n; j++) {
-            // 현재 학생의 선물만 반값 처리
-            if(j == i) {
-                tmp[j] = a[j][0]/2 + a[j][1];
-            } else {
-                tmp[j] = a[j][0] + a[j][1];
-            }
-        }
-        
-        // 가격 오름차순 정렬
-        sort(tmp, tmp+n);
-        
-        // 예산 내 최대 구매 가능 학생 수 계산
-        for(int j = 0; j < n; j++) {
-            if(tmpsum + tmp[j] <= b) {
+        tmp[i] = tmp[i] - a[i][0]/2;
+        sort(tmp,tmp+n);
+        for(int j = 0 ; j < n ; j++)
+        {        
+
+            if(tmpsum + tmp[j]<=b)
+            {
                 tmpsum += tmp[j];
                 cnt++;
             }
+
         }
-        
-        mx = max(mx, cnt);
+        tmp[i] = tmp[i]+ a[i][0]/2;
+        mx = max(mx,cnt);
     }
-    
     cout << mx;
     return 0;
 }
