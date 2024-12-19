1f2d89ada5fd66f227d224f6173177651742f8cb
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-12-19 13:47:09 +0900

    Title: 팰린드롬 수 찾기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241219/팰린드롬 수 찾기/find-the-number-of-palindrome.cpp
+++ 241219/팰린드롬 수 찾기/find-the-number-of-palindrome.cpp
@@ -6,24 +6,9 @@ int x,y;
 bool ispel(int i)
 {
     string s = to_string(i);
-    if(s.size() %2 == 0)
-    {
-       // 0 1 2 3 
-        string s1 = s.substr(0,s.size()/2);
-        string s2 = s.substr(s.size()/2);
-        reverse(s1.begin(),s1.end());
-        if(s1 == s2) return true;
-        // cout << s1 << " "<< s2 <<endl;
-    }
-    else
-    {
-        string s1 = s.substr(0,s.size()/2);
-        string s2 = s.substr(s.size()/2+1);
-        reverse(s1.begin(),s1.end());
-        if(s1 == s2) return true;   
-        // cout << s1 << " "<< s2 <<endl;
-
-    }
+    string s1 = s;
+    reverse(s.begin(), s.end());
+    if(s1 == s ) return true;
     return false;
 }
 
