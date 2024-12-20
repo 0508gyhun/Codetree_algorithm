2c30724a4c3c936425784627a21113704ca9efe5
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-12-20 10:37:14 +0900

    Title: 야바위, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241220/야바위/ya-rock.cpp
+++ 241220/야바위/ya-rock.cpp
@@ -16,7 +16,7 @@ int main() {
 
     }    
     int mx = INT_MIN;
-    for(int i = 1 ; i <= n ; i++)
+    for(int i = 1 ; i <= 3 ; i++)
     {
         int state[10] = {0,0,0,0};
         state[i] = 1;
