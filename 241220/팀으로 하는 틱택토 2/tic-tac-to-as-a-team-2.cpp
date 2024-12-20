cd9b339e60cbe276936545ecd03fa6127c2a4467
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-12-20 14:47:18 +0900

    Title: 팀으로 하는 틱택토 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241220/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.cpp
+++ 241220/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 int n =3;
 int a[3][3];
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-
     for(int i = 0 ; i < n ; i++)
     {
         string s;
@@ -16,32 +15,59 @@ int main() {
         }
     }
     int cnt = 0;
-
-    for(int i = 1 ; i < 10 ; i++)
-    {
-        for(int j = i+1 ; j < 10 ; j++)
-        {
-            //가로
-            for(int p = 0 ; p < 3 ; p++)
-            {
-                if( (a[p][0] == i || a[p][0] == j)&& (a[p][1] == i || a[p][1] == j) && (a[p][2] == i || a[p][2] == j)) cnt++;
-
-            }
-            //세로
-            for(int p = 0 ; p < 3 ; p++)
-            {
-                if( (a[0][p] == i || a[0][p] == j)&& (a[1][p] == i || a[1][p] == j) && (a[2][p] == i || a[2][p] == j)) cnt++;
-
-            }
-            if( (a[0][0] == i || a[0][0] == j)&& (a[1][1] == i || a[1][1] == j) && (a[2][2] == i || a[2][2] == j)) cnt++;
-            if( (a[0][2] == i || a[0][2] == j)&& (a[1][1] == i || a[1][1] == j) && (a[2][0] == i || a[2][0] == j)) cnt++;
-            
-            //세로
-
-            //대각
+    
+    //가로
+    for(int i = 0; i < 3; i++) {
+        int cntsum[10] = {0,};
+        for(int j = 0; j < 3; j++) {
+            cntsum[a[i][j]]++;
         }
+        int ans = 0;
+        for(int k = 1; k < 10; k++) {
+            if(cntsum[k] >= 1) ans++;
+        }
+        if(ans == 2) cnt++;
     }
-
+    
+    //세로
+    for(int j = 0; j < 3; j++) {
+        int cntsum[10] = {0,};
+        for(int i = 0; i < 3; i++) {
+            cntsum[a[i][j]]++;
+        }
+        int ans = 0;
+        for(int k = 1; k < 10; k++) {
+            if(cntsum[k] >= 1) ans++;
+        }
+        if(ans == 2) cnt++;
+    }
+    
+    //대각선1
+    {
+        int cntsum[10] = {0,};
+        cntsum[a[0][0]]++;
+        cntsum[a[1][1]]++;
+        cntsum[a[2][2]]++;
+        int ans = 0;
+        for(int i = 1; i < 10; i++) {
+            if(cntsum[i] >= 1) ans++;
+        }
+        if(ans == 2) cnt++;
+    }
+    
+    //대각선2
+    {
+        int cntsum[10] = {0,};
+        cntsum[a[0][2]]++;
+        cntsum[a[1][1]]++;
+        cntsum[a[2][0]]++;
+        int ans = 0;
+        for(int i = 1; i < 10; i++) {
+            if(cntsum[i] >= 1) ans++;
+        }
+        if(ans == 2) cnt++;
+    }
+    
     cout << cnt;
     return 0;
 }
