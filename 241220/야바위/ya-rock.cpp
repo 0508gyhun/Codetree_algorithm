10a4d66edcb5c5084076796041eb72d840a66b88
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-12-20 10:30:31 +0900

    Title: 야바위, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241220/야바위/ya-rock.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int a[104];
+int b[104];
+int c[104];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    cin >> n;
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin >> a[i] >> b[i] >> c[i];
+
+    }    
+    int mx = INT_MIN;
+    for(int i = 1 ; i <= n ; i++)
+    {
+        int state[4] = {0,0,0,0};
+        state[i] = 1;
+        int ans = 0 ;
+        for(int j= 0 ; j < n ; j++)
+        {   
+            int temp = state[a[j]];
+            state[a[j]] = state[b[j]];
+            state[b[j]] = temp;
+            if(state[c[j]] == 1) ans++;  
+               
+
+        }    
+        mx = max(mx,ans);
+    }
+    cout << mx;
+    return 0;
+}
