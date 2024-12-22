ce7f0a638eb2da63ebd4535aff040fd5a2a798fe
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2024-12-22 11:51:34 +0900

    Title: 독서실의 거리두기 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241222/독서실의 거리두기 4/study-cafe-keeping-distance-4.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+string s;
+
+int sol(string s) // 두사람의 가장 가까운 거리
+{
+
+    int mn = INT_MAX;
+    int dist = INT_MAX;
+    int prev = -1;
+    
+     for(int i = 0; i < s.size(); i++)
+    {
+        if(s[i] == '1')
+        {
+            if(prev >= 0)
+            {
+                dist = i - prev;
+                mn = min(mn, dist);
+            }
+            prev = i;
+        }
+    }
+    return mn;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >>n ;
+    cin >>s;
+
+    int mx = INT_MIN;
+    for(int i = 0 ; i < n ; i++)
+    {
+        for(int j = i+1 ; j < n ; j++)
+        {
+            if(s[i] == '1' || s[j] == '1') continue;
+
+            s[i] = '1';
+            s[j] = '1';
+
+
+            mx = max(mx,sol(s));
+
+            s[i] = '0';
+            s[j] = '0';
+
+        }
+    }
+    cout << mx;
+    return 0;
+}
