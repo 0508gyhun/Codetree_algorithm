a6c23783d517b175bc906f4fbad7349cc1869cf0
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2025-01-04 15:44:31 +0900

    Title: 행복한 수열의 개수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250104/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 250104/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 int n,m;
 int a[104][104];
+int seq[104];
+bool isgood()
+{
+    int prev = -1;
+    int mxcnt = 1;
+    int cnt = 1 ;
+    for(int i = 0 ; i <n; i++)
+    {
+        if(prev == seq[i]) cnt++;
+        else
+        {
+            cnt = 1;
+        }
+        mxcnt = max(mxcnt ,cnt);
+        
+        prev = seq[i];
+    }
+    return mxcnt>= m;
+
+}
 
 int main() {
     // Please write your code here.
@@ -20,33 +40,31 @@ int main() {
    int ans = 0;
     for(int i = 0 ; i< n ; i++)
     {
-        bool flag = false;
-        int prev = -1;
-        int cnt = 1;
+       
         for(int j =0 ; j < n ; j++)
         {
-            if(a[i][j] == prev) cnt++; 
-            prev = a[i][j];
-
+            seq[j] = a[i][j];
         }
-        if(cnt >= m) ans++; 
+        if(isgood())
+        {
+            ans++;
+        }
     }
 
   
+ 
     for(int i = 0 ; i< n ; i++)
     {
-        bool flag = false;
-        int prev = -1;
-        int cnt = 1;
+       
         for(int j =0 ; j < n ; j++)
         {
-            if(a[j][i] == prev) cnt++; 
-            prev = a[j][i];
-
+            seq[j] = a[j][i];
         }
-        if(cnt >= m) ans++; 
+        if(isgood())
+        {
+            ans++;
+        }
     }
-
     cout << ans;
     return 0;
 }
