eda0e282f8a5b3b7be721108074c4dc17778f9b5
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2025-01-05 17:39:54 +0900

    Title: 트로미노, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 250105/트로미노/tromino.cpp
+++ 250105/트로미노/tromino.cpp
@@ -8,8 +8,19 @@ int a[202][202];
 int sol1(int row, int col)
 {
     int sum = 0;
-    sum = (a[row][col]+ a[row+1][col] + a[row+1][col+1]);
-    return sum;
+    int mx = INT_MIN;
+    int sum1 = (a[row][col+1] + a[row+1][col] + a[row+1][col+1]);//ㄴ 반대
+    mx = max(mx,sum1);
+    int sum2 = (a[row][col]+ a[row+1][col] + a[row+1][col+1]); // ㄴ
+    mx = max(mx,sum2);
+
+    int sum3 = (a[row][col] + a[row][col+1] + a[row+1][col+1]);          
+    mx = max(mx,sum3);
+                                               //ㄱ
+    int sum4 = a[row][col] + a[row][col+1] + a[row+1][col];
+    mx = max(mx,sum4);
+
+    return mx;
     
 }
 
@@ -18,10 +29,15 @@ int sol2(int row, int col)
     int sum = a[row][col]+ a[row][col+1] + a[row][col+2];
     return sum;
 }
+int sol3(int row, int col)
+{
+    int sum = a[row][col]+ a[row+1][col] + a[row+2][col];
+    return sum;
+}
 
 int main() {
 
-    cin >> n , m;
+    cin >> n >> m;
     
     for(int i = 0 ; i < n ; i++)
     {
@@ -41,13 +57,34 @@ int main() {
 
         }
     }
-// 
+//  가로
     for(int i = 0 ; i < n ; i++)
     {
         for(int j = 0 ; j < m ; j++)
         {
-            if(j+2 >= n) continue;
+            if(j+2 >= m) continue;
             mx = max(mx,sol2(i,j));
+
+        }
+    }
+//가로 
+    for(int i = 0 ; i < n ; i++)
+    {
+        for(int j = 0 ; j < m ; j++)
+        {
+            if(j+2 >= m) continue;
+            mx = max(mx,sol2(i,j));
+
+        }
+    }
+
+    //세로
+    for(int i = 0 ; i < n ; i++)
+    {
+        for(int j = 0 ; j < m ; j++)
+        {
+            if(i+2 >= n) continue;
+            mx = max(mx,sol3(i,j));
 
         }
     }
