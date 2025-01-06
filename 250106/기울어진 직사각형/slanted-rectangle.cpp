b0813718dfff905469229025fc57a31340c460d9
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2025-01-06 12:15:46 +0900

    Title: 기울어진 직사각형, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250106/기울어진 직사각형/slanted-rectangle.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int n,m;
+int a[24][24];
+bool InRange(int x, int y) {
+    return 0 <= x && x < n && 0 <= y && y < n;
+}
+
+int sol(int row, int col, int l , int k)
+{
+    int dy[4] = {-1,-1,1,1};
+    int dx[4] = {1,-1,-1,1};
+    int dir[4] = {l,k,l,k};
+    
+    for(int d = 0 ; d < 4 ; d++)
+    {
+        for(int q = 0 ; q< dir[d] ; q++)
+        {
+            x+=dx[d];
+            y+=dy[d];
+
+            if(!InRange(y,x)) return 0;
+
+            sum+=a[y][x];
+        }
+    }
+    return sum;
+}
+
+
+int main() {
+    // Please write your code here.
+
+    cin >> n;
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        for(int j = 0 ; j < n ; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    int ans = 0 ;
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        for(int j = 0 ; j < n ; j++)
+        {
+            for(int l = 1 ; l < n ; l++)
+            {
+                for(int k = 1 ; k < n ; k++)
+                {
+                    ans = max(ans,sol(i,j,l,k));
+                }
+            }
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
