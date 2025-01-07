88cec968d63282844358dd8430f2679ca8749cb0
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2025-01-07 14:11:18 +0900

    Title: 기울어진 직사각형의 회전, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 250107/기울어진 직사각형의 회전/rotate-slanted-rectangle.cpp
+++ 250107/기울어진 직사각형의 회전/rotate-slanted-rectangle.cpp
@@ -1,138 +1,84 @@
 #include <iostream>
-#include <bits/stdc++.h>
+#include <vector>
 using namespace std;
+
 int n;
 int a[101][101];
-int r,c,m1,m2,m3,m4;
+int r, c, m1, m2, m3, m4;
 int dir;
-// / 행의 원소를 왼쪽으로 이동
-// void moveleft(int idx) {
-//     int temp = a[idx][0];
-//     for (int i = 0; i < m - 1; i++) {
-//         a[idx][i] = a[idx][i + 1];
-//     }
-//     a[idx][m - 1] = temp;
-// }
 
-// // 행의 원소를 오른쪽으로 이동
-// void moveright(int idx) {
-//     int temp = a[idx][m - 1];
-//     for (int i = m - 1; i > 0; i--) {
-//         a[idx][i] = a[idx][i - 1];
-//     }
-//     a[idx][0] = temp;
-// }
-void sol( int r , int c, int m1, int m2, int m3, int m4, int dir)
-{
-    int dy[4] = {-1,-1,1,1};
-    int dx[4] = {1,-1,-1,1};
-    int dr[4] = {m1,m2,m3,m4};
+void sol(int r, int c, int m1, int m2, int m3, int m4, int dir) {
+    // 이동 방향
+    int dy[4] = {-1, -1, 1, 1};
+    int dx[4] = {1, -1, -1, 1};
+    int dr[4] = {m1, m2, m3, m4};
 
-    int str = r;
-    int stc = c;
-    vector <int> v;
+    // 테두리 값 추출
+    int str = r, stc = c; // 시작점 저장
+    vector<int> v;
     v.push_back(a[r][c]);
-    for(int i = 0 ; i < 4. ; i++){
-        for(int j = 0 ; j < dr[i] ; j++)
-        {
-            r = r + dy[i];
-            c = c + dx[i];
+
+    for (int i = 0; i < 4; i++) { // 각 방향으로 이동
+        for (int j = 0; j < dr[i]; j++) {
+            r += dy[i];
+            c += dx[i];
             v.push_back(a[r][c]);
         }
     }
+
+    // 마지막 중복 값 제거
     v.pop_back();
 
-
-    // for(int i : v)
-    // {
-    //     cout << i<< " ";
-    // }
-    cout << endl;
-
-    if(dir == 0){ //오른쪽으로 밀어
-        int temp = v[v.size()-1];
-        for(int i = v.size()-1 ; i>0 ; i--)
-        {
-            v[i] = v[i-1];
+    // 방향에 따라 벡터를 이동
+    if (dir == 0) { // 오른쪽으로 밀기
+        int temp = v.back();
+        for (int i = v.size() - 1; i > 0; i--) {
+            v[i] = v[i - 1];
         }
         v[0] = temp;
-
-        // for(int i : v)
-        // {
-        //     cout << i<< " ";
-        // }
-        // cout << endl;
-    } // 왼쪽으로 밀어 
-    else if(dir == 1){
+    } else if (dir == 1) { // 왼쪽으로 밀기
         int temp = v[0];
-        for(int i = 0 ; i<v.size()-1 ; i++)
-        {
-            v[i] = v[i+1];
+        for (int i = 0; i < v.size() - 1; i++) {
+            v[i] = v[i + 1];
         }
-
-        v[v.size()-1] = temp;
-
-        // for(int i : v)
-        // {
-        //     cout << i<< " ";
-        // }
-        // cout << endl;
+        v[v.size() - 1] = temp;
     }
 
+    // 테두리 값 갱신
+    r = str; c = stc; // 시작점 복원
+    a[r][c] = v[0];
+    int idx = 1;
 
-// int dy[4] = {-1,-1,1,1};
-//     int dx[4] = {1,-1,-1,1};
-//     int dr[4] = {m1,m2,m3,m4};
-
-//     vector <int> v;
-//     v.push_back(a[r][c]);
-//     for(int i = 0 ; i < 4. ; i++){
-//         for(int j = 0 ; j < dr[i] ; j++)
-//         {
-//             r = r + dy[i];
-//             c = c + dx[i];
-//             v.push_back(a[r][c]);
-//         }
-//     }
-//     v.pop_back();
-    int idx = 0;
-    a[str][stc] = v[idx++];
-    for(int i = 0 ; i < 4. ; i++){
-        for(int j = 0 ; j < dr[i] ; j++)
-        {
-            str = str + dy[i];
-            stc = stc + dx[i];
-            if(a[str][stc] != 0) a[str][stc] = v[idx++];
+    for (int i = 0; i < 4; i++) { // 각 방향으로 이동
+        for (int j = 0; j < dr[i]; j++) {
+            r += dy[i];
+            c += dx[i];
+            if(idx != v.size()) a[r][c] = v[idx++];
         }
     }
 
-     for(int i = 0 ; i < n ; i++)
-    {
-        for(int j = 0 ; j < n ; j++)
-        {
-            cout << a[i][j] <<" ";
+    // 결과 출력
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << a[i][j] << " ";
         }
         cout << endl;
     }
-
-
 }
+
 int main() {
-    // Please write your code here.
-    
-    cin >> n ; 
-    for(int i = 0 ; i < n ; i++)
-    {
-        for(int j = 0 ; j < n ; j++)
-        {
+    cin >> n; // 격자 크기 입력
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cin >> a[i][j];
         }
     }
 
+    // 입력: 시작점, 각 변의 길이, 방향
     cin >> r >> c >> m1 >> m2 >> m3 >> m4 >> dir;
-    r--;
-    c--;
-    sol(r,c,m1,m2,m3,m4,dir);
+    r--; c--; // 1-based index를 0-based로 변환
+
+    sol(r, c, m1, m2, m3, m4, dir);
 
     return 0;
 }
