1cb9d9d7ac2f967c583190d96f6d021a33ae8573
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2025-01-10 15:06:11 +0900

    Title: 2차원 폭발 게임, Time: 11ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250110/2차원 폭발 게임/The-2D-bomb-game.cpp
+++ 250110/2차원 폭발 게임/The-2D-bomb-game.cpp
@@ -161,6 +161,11 @@ int main() {
     {
         bomb();
         down();
+        while(ispossible())
+        {
+            bomb();
+            down();
+        }
         rotateRight();
         down();
     }
