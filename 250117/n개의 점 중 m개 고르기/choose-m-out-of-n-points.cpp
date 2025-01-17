0b68985fc1e658aeeddb4e805c5f3001c0ea2837
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2025-01-17 20:01:30 +0900

    Title: n개의 점 중 m개 고르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250117/n개의 점 중 m개 고르기/choose-m-out-of-n-points.cpp
+++ 250117/n개의 점 중 m개 고르기/choose-m-out-of-n-points.cpp
@@ -6,61 +6,62 @@ using namespace std;
 int n, m;
 int x[20], y[20];
 vector<int> v;  // n개 중 m개 선택한 점들의 인덱스 저장
-vector<int> v2; // m개 중 2개 선택한 점들의 인덱스 저장
+vector<int> selected;  // m개 중 2개 선택한 실제 인덱스
 int mn = INT_MAX;
 
-int dif(int n1, int n2) {
-    int s1 = (x[n1]-x[n2]) * (x[n1]-x[n2]);
-    int s2 = (y[n1]-y[n2]) * (y[n1]-y[n2]);
+// 두 점 사이의 거리 제곱 계산
+long long dif(int n1, int n2) {
+    long long s1 = (long long)(x[n1] - x[n2]) * (x[n1] - x[n2]);
+    long long s2 = (long long)(y[n1] - y[n2]) * (y[n1] - y[n2]);
     return s1 + s2;
 }
 
-int cal() {
-    int n1 = v2[0];
-    int n2 = v2[1];
-    return dif(n1,n2);
+// 선택된 두 점 사이의 거리 계산
+long long cal() {
+    return dif(selected[0], selected[1]);
 }
 
+// m개 중 2개 선택
 void sol2(int idx, int cnt) {
-    if(idx == v.size()) {
-        if(cnt == 2) {
-            mn = min(mn,cal());
-        }
+    if(cnt == 2) {
+        mn = min(mn, (int)cal());
         return;
     }
     
-    v2.push_back(v[idx]);
-    sol2(idx+1,cnt+1);
-    v2.pop_back();
-    
-    sol2(idx+1,cnt);
+    for(int i = idx; i < v.size(); i++) {
+        selected.push_back(v[i]);
+        sol2(i + 1, cnt + 1);
+        selected.pop_back();
+    }
 }
 
 void print() {
-    sol2(0,0);
+    selected.clear();
+    sol2(0, 0);
 }
 
+// n개 중 m개 선택
 void sol(int idx, int cnt) {
-    if(idx == n) {
-        if(cnt == m) print();
+    if(cnt == m) {
+        print();
         return;
     }
     
-    v.push_back(idx);
-    sol(idx+1, cnt+1);
-    v.pop_back();
-    
-    sol(idx+1, cnt);
+    for(int i = idx; i < n; i++) {
+        v.push_back(i);
+        sol(i + 1, cnt + 1);
+        v.pop_back();
+    }
 }
 
 int main() {
     cin >> n >> m;
     
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         cin >> x[i] >> y[i];
     }
     
-    sol(0,0);
+    sol(0, 0);
     cout << mn;
     
     return 0;
