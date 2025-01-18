3d013bbef6cf7fd3cd85d2a69e4b701ba2a87183
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2025-01-18 15:15:38 +0900

    Title: 수들의 합 최대화하기, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 250118/수들의 합 최대화하기/max-sum-of-numbers.cpp
+++ 250118/수들의 합 최대화하기/max-sum-of-numbers.cpp
@@ -1,62 +1,53 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-
-#define MAX_N 10
+#include <bits/stdc++.h>
 
 using namespace std;
 
-// 변수 선언
 int n;
-int grid[MAX_N][MAX_N];
-bool visited[MAX_N];
-vector<int> picked;
+int a[10][10];
+vector<int> v;
+int visited[11];
 
-int ans;
-
-// 현재 색칠된 칸을 선택할 행을 row라 했을 때 
-// 계속 탐색을 이어서 진행합니다.
-// 첫 번째 행에 색칠할 열의 위치부터
-// 두 번째, 세 번째, .. n번째 행에 색칠할 열의 위치까지
-// 각 열의 위치에 대한 순열을 만들어줍니다.
-void FindMax(int row) {
-    // 색칠된 칸에 적힌 수들의 합 중
-    // 최댓값을 갱신합니다.
-    if(row == n) {
-        int sum = 0;
-        for(int i = 0; i < n; i++)
-            sum += grid[i][picked[i]];
-
-        // 답을 갱신해줍니다.
-        ans = max(ans, sum);
-        return;
+int mx = 0;
+void sol(int rowidx)
+{
+    if (rowidx == n)
+    {   int sum = 0 ;
+        for(int i = 0 ; i < n ; i++)
+        {
+            sum+=a[i][v[i]];
+        }
+        mx = max(mx,sum);
     }
-
-    // 현재 행에 색칠할 열을 선택합니다.
-    for(int i = 0; i < n; i++) {
+    // 0 - n    까지의 어떤 열에 수를 선캑할 것인지.
+    for(int i = 0 ; i < n ; i++)
+    {
         if(visited[i]) continue;
+
+        v.push_back(i);
         visited[i] = true;
-        picked.push_back(i);
 
-        FindMax(row + 1);
+        sol(rowidx+1);
 
+        v.pop_back();
         visited[i] = false;
-        picked.pop_back();
+
     }
 }
 
+
+
 int main() {
-    // 입력:
     cin >> n;
 
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            cin >> grid[i][j];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
+    sol(0);
+    // Write your code here!
+    cout << mx;
 
-    // 합이 최대가 되도록
-    // 탐색을 진행합니다.
-    FindMax(0);
-
-    cout << ans;
     return 0;
 }
