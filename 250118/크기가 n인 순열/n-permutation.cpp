3b93b73c0f2ab3710345eaa12e691a6e6b515ba9
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2025-01-18 12:24:24 +0900

    Title: 크기가 n인 순열, Time: 89ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 250118/크기가 n인 순열/n-permutation.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int visited[9];
+vector <int> v;
+void print()
+{
+    for(int i: v)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+void sol(int idx)
+{
+ 
+    if(idx == n)
+    {
+        print();
+        return ;
+    }
+
+    for(int i = 1 ; i <=n ; i++)
+    {
+        if(visited[i] == true)
+        {
+            continue;
+        }
+        v.push_back(i);
+        visited[i] = true;
+
+        sol(idx+1);
+
+        v.pop_back();
+        visited[i] = false;
+    }
+}
+
+
+int main() {
+    cin >> n;
+
+
+    sol(0);
+    // Write your code here!
+
+    return 0;
+}
+//1-n 을 사용해서 수열, 사전순 , 오름차순..
