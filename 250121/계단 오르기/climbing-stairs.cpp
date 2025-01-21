1640ed92f1e98dac9e6e17efb5b5f5542d90a34b
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2025-01-21 16:07:54 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250121/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int dp[1004];
+int n;
+
+int main() {
+    cin >> n;
+
+    dp[0] = 1;
+    dp[1] = 0;
+    dp[2] = 1;
+    dp[3] = 1;
+
+
+    for(int i = 4 ; i <= n ;i++ )  dp[i] = dp[i-2] + dp[i-3];
+
+    cout << dp[n];
+    // Write your code here!
+
+    return 0;
+}
