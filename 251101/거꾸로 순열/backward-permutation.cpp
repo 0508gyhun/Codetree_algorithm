a706dc68b98b30ef6ff62f3d23e2c493cccd371e
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2025-11-01 20:36:54 +0900

    Title: 거꾸로 순열, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251101/거꾸로 순열/backward-permutation.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> v;
+bool visited[9];
+int n;
+
+void prt() {
+    for(int i : v) {
+        cout << i <<" ";
+    }
+    cout << endl;
+}
+
+void go(int idx) {
+    if(idx == n) {
+        prt();
+        return;
+    }
+
+    for(int i = 9 ; i <= 1 ; i++) {
+        if(visited[i]) continue;
+        visited[i] = true;
+        v.push_back(i);
+
+        go(idx+1);
+        
+        visited[i] = false;
+        v.pop_back();    
+    }
+}
+
+
+int main() {
+    cin >> n;
+
+    // Please write your code here.
+    go(0);
+    return 0;
+}
