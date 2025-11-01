3034b05955173c6e75f120263e9535990b7486fb
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2025-11-01 16:32:02 +0900

    Title: N개의 점 중 M개 고르기, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 251101/N개의 점 중 M개 고르기/choose-m-out-of-n-points.cpp
+++ 251101/N개의 점 중 M개 고르기/choose-m-out-of-n-points.cpp
@@ -13,18 +13,32 @@ vector<pair<int,int>> v;
 // n개 중 M 개 고르기
 // m 중 처음과 끝 거리 게산
 int mn = INT_MAX;
+
+int cal(int i , int j) {
+    int st1 = v[i].first;
+    int ed1 = v[i].second;
+
+    int st2 = v[j].first;
+    int ed2 = v[j].second;
+
+    return (st2-st1)*(st2-st1) + (ed2-ed1)*(ed2-ed1);
+}
+
 void prt() {
     // for(auto i : v) {
     //     cout << i.first<<" "<<i.second<< endl;
     // }
     // cout<< endl;
-    pair<int,int> st = v[0];
-    pair<int,int> ed = v[m-1];
+    int mx = INT_MIN;
 
-    int dist = ((st.first-ed.first)*(st.first-ed.first) + (st.second - ed.second)*(st.second - ed.second));
-
-    mn = min(mn,dist);
-
+   
+    for(int i = 0 ; i < m ; i ++) {
+        for(int j = i+1 ; j < m ; j++) {
+            //가장 먼 곳
+            mx = max(mx,cal(i,j));
+        }
+    }
+    mn = min(mn,mx);
 }
 
 void go(int idx, int cnt) {
@@ -51,7 +65,7 @@ int main() {
         cin >> x[i] >> y[i];
         nums.push_back({x[i],y[i]});
     }
-    sort(nums.begin(), nums.end());
+
 
     go(0,0);
 
