66c77757e99e97f1c993fed47b60ba083e1f7950
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2025-12-02 11:49:35 +0900

    Title: 나이트, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251202/나이트/knight-movements.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int r1, c1, r2, c2;
+queue<pair<int,int>> q;
+int visited[104][104];
+int dist[104][104];
+
+int dy[8] = {-1,-2,-2,-1,1,2,2,1};
+int dx[8] = {-2,-1,1,2,2,1,-1,-2};
+
+bool inRange(int y, int x)
+{
+    return (x >= 0 && x < n && y >=0 && y < n);
+}
+void bfs() {
+
+    while(q.size()) {
+        int y,x;
+        tie(y,x) = q.front();
+        q.pop();
+
+        for(int i = 0 ; i < 8 ; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if(visited[ny][nx] == 0 && inRange(ny,nx))
+            {
+                dist[ny][nx] = dist[y][x] + 1;
+                visited[ny][nx] = 1;
+                q.push({ny,nx});
+            }
+        }
+    }
+}
+
+int main() {
+    cin >> n;
+    cin >> r1 >> c1 >> r2 >> c2;
+    r1--;
+    c1--;
+    // Please write your code here.
+    q.push({r1,c1});
+    visited[r1][c1] = 1;
+    dist[r1][c1] = 0;
+    bfs();
+
+    cout << dist[r2-1][c2-1];
+    return 0;
+}
