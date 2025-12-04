4fac0a8b60287d08e1e8194dfc2352e53e548d67
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2025-12-04 18:06:52 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251204/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
+++ 251204/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
@@ -31,7 +31,7 @@ int main() {
             if(i == 1) num = num +1;
             if(i == 2 && num % 2 == 0) num = num /2;
             if(i == 3 && num % 3 == 0) num = num /3;
-            if(num>=1 && num <= 1000000 &&visited[num] == 0) 
+            if(num>=0 && num <= 1000000 &&visited[num] == 0) 
             {
                 visited[num] = 1;
                 q.push({num, cnt+1});
