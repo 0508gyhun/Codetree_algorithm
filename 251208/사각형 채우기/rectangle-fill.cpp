6e5fb6b40e9e824ffdba34c81ffde590772ac2c9
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2025-12-08 14:44:24 +0900

    Title: 사각형 채우기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251208/사각형 채우기/rectangle-fill.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int dp[1004] ;
+int n;
+
+int main() {
+    cin >> n;
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 3;
+
+    for(int i = 4 ; i <= n ; i++) 
+    {
+        dp[i] = (dp[i-1] + dp[i-2])%10007;
+    }
+
+    cout<<dp[n];
+    // Please write your code here.
+
+    return 0;
+}
