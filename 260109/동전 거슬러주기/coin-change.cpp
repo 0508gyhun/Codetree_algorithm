00bab468a70b21a1719da9fb269b2ffd4855cb3e
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2026-01-09 19:06:33 +0900

    Title: 동전 거슬러주기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260109/동전 거슬러주기/coin-change.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+int coin[100];
+int dp[104];
+int main() {
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        cin >> coin[i];
+    }
+
+     for(int i = 0 ; i <= M ; i++)
+    {
+        dp[i] = INT_MAX;
+    }
+    dp[0] = 0;
+
+
+
+
+    for(int i = 1 ; i <= M ; i++)
+    {
+        for(int j = 0 ; j < N ; j++) 
+        {
+            if(dp[i-coin[j]] == INT_MAX) continue;
+
+            dp[i] = min(dp[i], dp[i-coin[j]] + 1);
+        }
+    }
+
+    cout << dp[M];
+
+    // Please write your code here.
+
+    return 0;
+}
