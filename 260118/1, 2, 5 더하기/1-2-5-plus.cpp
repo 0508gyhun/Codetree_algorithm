b45e87af567434b4dab0f3e11f3b5459e9375e45
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2026-01-18 18:45:20 +0900

    Title: 1, 2, 5 더하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260118/1, 2, 5 더하기/1-2-5-plus.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+ // 1 2 5 중복 가능으로 N 만드는 법
+int m;
+
+int n = 3;
+int A[3] = {1,2,5};
+int dp[10000];
+int main() {
+    cin >> m;
+
+    for(int i = 0 ; i < m ; i++) 
+    {
+        dp[i] = 0;
+    }
+    dp[0] = 1;
+
+    for(int i = 1 ; i <= m ;i ++) {
+        for(int j = 0 ; j < n ; j++)
+        {
+            if(i  - A[j] >= 0 )
+            {
+                dp[i] = dp[i] + dp[i-A[j]];
+            }
+        }
+        
+    }
+
+    cout << dp[m];
+
+    // Please write your code here.
+
+    return 0;
+}
