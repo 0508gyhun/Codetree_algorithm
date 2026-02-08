0538b0c4055158d2bdcac4bd346faefd7b344bb8
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2026-02-08 15:27:55 +0900

    Title: 동전 거슬러주기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260208/동전 거슬러주기/coin-change.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+int coin[100];
+const int INF = 100000000;
+int dp[100004];
+int main() {
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        cin >> coin[i];
+    }
+
+     for(int i = 0 ; i <= M  ; i++)
+    {
+        dp[i] = INF;
+    }
+    dp[0] = 0;
+
+
+
+
+    for(int i = 1 ; i <= M ; i++)
+    {
+        for(int j = 0 ; j < N ; j++) 
+        {
+            // if(dp[i-coin[j]] == INT_MAX) continue;
+            if(i-coin[j] >= 0 && dp[i-coin[j]] != INT_MAX) dp[i] = min(dp[i], dp[i-coin[j]] + 1);
+        }
+    }
+    if(dp[M] == INF) cout <<-1;
+    else cout << dp[M];
+
+    // Please write your code here.
+
+    return 0;
+}
