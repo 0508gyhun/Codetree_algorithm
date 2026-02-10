e13bd19fbc12357ee1011528a7c5879fa684ddd0
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2026-02-10 20:17:04 +0900

    Title: 부분 수열의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260210/부분 수열의 합/the-sum-of-the-subsequences.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+ // 중복 허용 x , 동전
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int A[100];
+int dp[10004];
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+    for(int i = 0 ; i <= m ; i++)
+    {
+        dp[i] = INT_MIN;
+    }
+    dp[0] = 1;
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        for(int j = m ; j >=0 ; j--)
+        {
+            if(i - A[j] >= 0 && dp[i - A[j] != INT_MIN)
+            {
+                dp[j] = max(dp[j], dp[i - A[j]]+1);
+            }
+        }
+    }
+    for(int i = 0 ; i <= m ; i++)
+    {
+        cout << dp[i]<<" ";
+    }
+
+    if(dp[m] == INT_MIN) cout << "No";
+    else cout << "Yes";
+
+    // Please write your code here.
+
+    return 0;
+}
