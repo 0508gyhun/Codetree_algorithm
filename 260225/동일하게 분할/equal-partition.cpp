bc442510a2df1ebc2faf00b7671fa8531d4931d9
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2026-02-25 15:28:27 +0900

    Title: 동일하게 분할, Time: 11ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 260225/동일하게 분할/equal-partition.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n,m;
+int arr[100];
+bool dp[104][100004];
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        m += arr[i];
+    }
+    dp[0][0] = true;
+    for(int i = 1 ; i <= n ; i++)
+    {
+        for(int j = 0 ; j <= m ; j++)
+        {
+            if(j - arr[i] >= 0 && dp[i-1][j-arr[i]])
+            {
+                dp[i][j] = true;
+            }
+            if(dp[i-1][j])
+            {
+                dp[i][j] = true;
+            }
+        }
+    }
+    if (m % 2 == 0 && dp[n][m/2]) {
+        cout << "Yes";
+    } else {
+        cout << "No";
+    }
+    // Please write your code here.
+
+    return 0;
+}
