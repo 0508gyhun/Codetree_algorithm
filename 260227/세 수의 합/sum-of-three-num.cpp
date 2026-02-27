6cb70113ea00117284e3d7ca5663a528884cdb0f
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2026-02-27 14:27:13 +0900

    Title: 세 수의 합, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260227/세 수의 합/sum-of-three-num.cpp
+++ 260227/세 수의 합/sum-of-three-num.cpp
@@ -1,48 +1,39 @@
 #include <iostream>
-#include <vector>
-#include <unordered_map>
+#include <bits/stdc++.h>
+
 
 using namespace std;
 
+int n, k;
+int arr[1004];
+
 int main() {
-    // 입출력 속도 향상 (C++ 알고리즘 풀이 시 필수)
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int n, k;
     cin >> n >> k;
-
-    vector<int> arr(n);
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
+    int total = 0;
+    for(int i = 0 ; i < n ; i++)
+    {
+        int target = k - arr[i];
 
-    long long total_count = 0; // 경우의 수가 매우 커질 수 있으므로 long long 사용
+        unordered_map <int,int> um;
 
-    // 1. 첫 번째 숫자를 고정합니다.
-    for (int i = 0; i < n; i++) {
-        int target = k - arr[i]; // 나머지 두 수의 합 목표치
-        
-        // 2. 지나온 숫자의 빈도수를 기록할 해시맵 (장부)
-        unordered_map<int, int> history;
-        
-        // 3. i번 위치 뒤에 있는 숫자들을 탐색하며 2개를 마저 고릅니다.
-        for (int j = i + 1; j < n; j++) {
-            int needed = target - arr[j]; // target을 만들기 위해 필요한 짝꿍 숫자
-            
-            // 장부에 짝꿍 숫자가 기록되어 있는지 확인
-            // C++에서는 count() 함수로 키(key)의 존재 여부를 알 수 있습니다.
-            if (history.count(needed) > 0) {
-                total_count += history[needed]; // 찾았다면 그 개수만큼 정답에 더함
+        for(int j = i+1 ; j < n ; j++)
+        {
+            int need = target - arr[j];
+
+            if(um.count(need) > 0 )
+            {
+                total += um.count(need);
             }
-            
-            // 현재 숫자 arr[j]도 다음 탐색을 위해 장부에 기록
-            // C++의 map은 없는 키에 접근해 ++를 하면 자동으로 1로 초기화해 줍니다.
-            history[arr[j]]++;
-        }
+
+            um[arr[j]]++;
+        } 
     }
 
-    cout << total_count << "\n";
+    cout <<total;
+    // Please write your code here.
 
     return 0;
 }
