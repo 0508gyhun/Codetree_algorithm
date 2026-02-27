2a568edfd9f56c4071b8658c24d5ee8f6ceeecff
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2026-02-27 12:00:27 +0900

    Title: 세 수의 합, Time: 21ms, Memory: 0MB, Status: Passed - Codetree

--- 260227/세 수의 합/sum-of-three-num.cpp
+++ 260227/세 수의 합/sum-of-three-num.cpp
@@ -1,20 +1,4 @@
 #include <iostream>
-
-using namespace std;
-
-int n, k;
-int arr[1000];
-
-int main() {
-    cin >> n >> k;
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    // Please write your code here.
-
-    return 0;
-}#include <iostream>
 #include <vector>
 #include <unordered_map>
 
