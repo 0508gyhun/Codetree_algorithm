687394383dc6f29fa4a0ba4c62df233bede309f0
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2026-02-28 18:34:05 +0900

    Title: 정수 찾기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260228/정수 찾기/find-an-integer.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int m;
+int a[100000];
+int b[100000];
+
+int main() {
+    cin >> n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr1[i];
+        s.insert(arr1[i]);
+    }
+
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> arr2[i];
+        if(s.find(arr2[i]) != s.end())
+        {
+            cout << "1"<<" ";
+        }
+        else
+        {
+            cout << "0"<<" ";
+        }
+    }
+
+
+    // Please write your code here.
+
+    return 0;
+}
