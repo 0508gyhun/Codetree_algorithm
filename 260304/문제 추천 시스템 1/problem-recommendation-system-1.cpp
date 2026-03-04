2493484b83ca1c371019f0853321f932dc050a70
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2026-03-04 13:31:31 +0900

    Title: 문제 추천 시스템 1, Time: 97ms, Memory: 3MB, Status: Passed - Codetree

---
+++ 260304/문제 추천 시스템 1/problem-recommendation-system-1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int P[100000], L[100000];
+set <pair<int,int>> s;
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> P[i] >> L[i];
+        s.insert({L[i],P[i]});
+    }
+
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        string command;
+        cin >> command;
+
+        if (command == "rc") {
+            int x;
+            cin >> x;
+            if(x == 1)
+            {
+                auto it = s.rbegin();
+                cout << (*it).second << endl;
+            }
+            else if( x == -1)
+            {
+                auto it = s.begin();
+                cout << (*it).second << endl;
+            }
+        } else if (command == "ad") {
+            int p, l;
+            cin >> p >> l;
+            s.insert({l,p});
+        } else if (command == "sv") {
+            int p, l;
+            cin >> p >> l;
+            s.erase({l,p});
+        }
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
