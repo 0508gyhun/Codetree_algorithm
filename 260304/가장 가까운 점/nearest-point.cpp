b426ea97c2031836a3c33ac3fdcc02176d661949
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2026-03-04 20:01:32 +0900

    Title: 가장 가까운 점, Time: 73ms, Memory: 2MB, Status: Passed - Codetree

--- 260304/가장 가까운 점/nearest-point.cpp
+++ 260304/가장 가까운 점/nearest-point.cpp
@@ -7,54 +7,34 @@ const int MAX_N = 100000;
 int n, m;
 int x[MAX_N], y[MAX_N];
 
-priority_queue<pair<int,int>> pq;
-priority_queue<pair<int,int>> pq2;
+priority_queue<tuple<int,int,int>> pq;
+
 int main() {
     cin >> n >> m;
 
     for (int i = 0; i < n; i++) {
         cin >> x[i] >> y[i];
-        pq.push({-x[i],-y[i]});
+        pq.push({-(x[i] + y[i]), -x[i], -y[i]});
     }
 
     for(int i = 0; i < m ; i++)
     {
-        int a = pq.top().first;
-        int b = pq.top().second;
+        int ig,a,b;
+        tie(ig,a,b) = pq.top();
+        a = -a;
+        b = -b;
         pq.pop();
-        pq.push({a-2,b-2});
+        a+=2;
+        b+=2;
+        pq.push(make_tuple(-(a+b),-a,-b));
     }
 
-    while (!pq.empty()) {
-    int a = pq.top().first;
-    int b = pq.top().second;
-    pq.pop();
     
-    // pq에 -를 붙여서 넣었으므로, 다시 -를 붙이면 원래 양수값이 됩니다.
-    pq2.push({-a, -b});
-    }
-
-    // int c = pq.top().first;
-    // int d = pq.top().second;
-
     // cout <<-c + (-d) <<endl;
-    pair<int,int> ansp ={0,0};
-    int ans = INT_MAX;
-    for(int i = 0 ; i < n ;i++)
-    {
-        int a = pq2.top().first;
-        int b = pq2.top().second;
-        pq2.pop();
-        if(ans > a+b)
-        {
-            ans = a+b;
-            ansp.first = a;
-            ansp.second = b;
-        }
-    }
-    cout << ansp.first<<" "<<ansp.second<<endl;
-
-
+  
+  int lastx,lasty,ig;
+  tie(ig,lastx,lasty) = pq.top();
+  cout << -lastx <<" "<<-lasty;
 
     // Please write your code here.
 
