7797e6bce4ee2d9cd14954a19dcc51864a96a44b
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2026-03-05 13:38:46 +0900

    Title: 앞에서부터 삭제하기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260305/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
+++ 260305/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
@@ -5,38 +5,35 @@ using namespace std;
 
 int N;
 int arr[100000];
-queue<int> q;
+priority_queue <int> pq;
+int sum, max_avg;
 
 int main() {
     cin >> N;
     for (int i = 0; i < N; i++) {
         cin >> arr[i];
-        q.push(arr[i]);
     }
-    double ans = INT_MIN;
-    for(int i = 1 ; i <= N-2 ; i++)
+
+    pq.push(-arr[N-1]);
+    sum+=arr[N-1];
+
+    for(int i = N-2 ; i >=1 ; i --)
     {
-        q.pop();
-        queue <int> q1;
-        q1 = q;
-        priority_queue <int> pq;
-        while(q1.size())
+        pq.push(-arr[i]);
+        sum+=arr[i];
+
+        double avg = (double) (sum - (-pq.top()) / N-i-1);
+
+        if(max_avg <avg)
         {
-            int a = q1.front();
-            q1.pop();
-            pq.push(-a);
+            max_avg = avg;
         }
-        pq.pop(); // 가장 작은 숫자 제외
-        int pqs = pq.size();
-        long long total = 0;
-        while(pq.size())
-        {
-            total += (-pq.top());
-            pq.pop();
-        }
-        ans = max(ans, (double)total / pqs );
     }
-    cout << fixed << setprecision(2) << ans ;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << max_avg;
     
     // Please write your code here.
 
