903dd2b3bc149c05501ee0262a00be2fce037f61
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2026-03-09 13:36:15 +0900

    Title: 정수 N개의 합 2, Time: 32ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 260309/정수 N개의 합 2/sum-of-n-integers-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+int arr[100000];
+int psum[100004];
+
+int main() {
+    cin >> n >> k;
+
+    psum[0] = 0;
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+        psum[i] = psum[i-1]+ arr[i];
+    }
+    int ans = INT_MIN;
+    for(int i = n ; i - k >= 0 ;i--)
+    {
+        ans = max(ans,psum[i]-psum[i-k]);
+    }
+    cout << ans;
+
+    // Please write your code here.
+
+    return 0;
+}
