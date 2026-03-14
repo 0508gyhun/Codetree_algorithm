220bd74c5b1af841a27f3adbc43cfa2f806ca424
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2026-03-14 13:30:40 +0900

    Title: 가장 많이 겹치는 구간 2, Time: 17ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260314/가장 많이 겹치는 구간 2/section-with-maximum-overlap-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int x1[100004], x2[100004];
+int arr[100004];
+//1000 000 000
+int mx = INT_MIN;
+int ans = INT_MIN;
+int sum;
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x1[i] >> x2[i];
+        arr[x1[i]]++;
+        arr[x2[i]]--;
+
+        mx = max(mx,x2[i]);
+    }
+
+    for(int i = 1 ; i <= mx ; i++)
+    {
+        sum+=arr[i];
+        ans = max(ans,sum);
+    }
+    cout << ans;
+
+
+
+    // Please write your code here.
+
+    return 0;
+}
