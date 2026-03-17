94b8f36567d2bcb68375118918ca23a4500f19a8
0508gyhun <63939717+0508gyhun@users.noreply.github.com>
2026-03-17 09:03:31 +0900

    Title: 호텔 예약, Time: 200ms, Memory: 8MB, Status: Passed - Codetree

---
+++ 260317/호텔 예약/reserve-hotel.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int s[100004];
+int e[100004];
+
+vector<tuple<int,int,int>> v;
+
+map<int,int> mp;
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> s[i] >> e[i];
+        mp[s[i]]++;
+        mp[e[i]+1]--;
+    }
+
+    // sort(v.begin(), v.end());
+    // set<int> seg;
+    int sum = 0 ;
+    int ans = -1;
+    int prev = -1;
+    for(auto i : mp)
+    {
+        
+        sum += i.second;
+        // if(prev == i.first) sum++;
+        ans = max(ans,sum);
+        // prev = i.first;
+    }
+
+    cout << ans;
+
+    
+        // Please write your code here.
+
+    return 0;
+}
